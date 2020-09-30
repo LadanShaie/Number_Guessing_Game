1dd28ed2bdfcf8c419471c08c20c6b2c0a0fcf14
vagrant <vagrant@vagrant.vm>
2020-09-30 00:32:02 +0000

    Number-guessing game

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
-import random
+import random #Choose random number 
 """A number-guessing game."""
-print ("Welcome To The Numbers Game!") #greeting 
-answer =input("What is your name")
-print(answer + ", I'm thinking of a number between 1 and 100")
+print ("Welcome To Guess The Number Game!") #Player greeting 
+answer =input("Enter your name: ") #Get player name
+print(answer+ ", I'm thinking of a number between 1 and 100")
 number =random.randint(1,100)
 
 num = int (input("Try to guess my number: "))
@@ -12,15 +12,15 @@ while number != num:
     if number < num: 
         print ("your guess is too high, try again." )
         num = int(input("your guess? " ))
-        guess_attempts= guess_attempts + 1
+        guess_attempts += 1 #equivalent to guess_attempts= guess_attempts + 1
     elif num < number: 
         print ("your guess is too low, try again." ) 
         num = int(input("your guess? " ))
-        guess_attempts= guess_attempts + 1
+        guess_attempts += 1
 else:
-    guess_attempts= guess_attempts + 1
+    guess_attempts += 1
     string_guess_attempts= str(guess_attempts)
-    print ("Well done, "+answer+"You found my number in "+string_guess_attempts+" tries!")        
+    print ("Well done, "+answer+"! You found my number in "+string_guess_attempts+" tries!")        
   
         
 
@@ -34,4 +34,3 @@ else:
 
 
 
-#print ("hi")
